25353174ca42f883e4a19a25c21c1616d312ec18
trdiaz <tomiasdays@gmail.com>
2019-11-27 23:25:31 -0700

    Update problemThree.java

--- problem-3/problemThree.java
+++ problem-3/problemThree.java
@@ -1,16 +1,16 @@
-
 class ProblemThree {
-    public int countSubstrings(String S) {
-        int N = S.length(), ans = 0;
+    public int countSubstrings(String palindrome) {
+        int N = palindrome.length()
+        int numPalin = 0;
         for (int center = 0; center <= 2*N-1; ++center) {
             int left = center / 2;
             int right = left + center % 2;
-            while (left >= 0 && right < N && S.charAt(left) == S.charAt(right)) {
-                ans++;
-                left--;
+            while (left >= 0 && right < N && palindrome.charAt(left) == palindrome.charAt(right)) {
+                numPalin++;
                 right++;
+                left--;
             }
         }
-        return ans;
+        return numPalin;
     }
 }
